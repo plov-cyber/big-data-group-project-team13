2cc569159ff2a88686aa2564862d756edeef8d98
team13 <team13@hadoop-01.uni.innopolis.ru>
2024-04-08 19:59:16 +0300

    Add dataset downloading to stage1.sh

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+url="https://disk.yandex.ru/d/ErRH7JdGugYe8A"
+
+wget "$(yadisk-direct $url)" -O data/data.zip
+
+unzip data/data.zip -d data/
+cp data/ibd_group_project/*.csv data/
+rm -rf data/ibd_group_project
+rm data/data.zip
