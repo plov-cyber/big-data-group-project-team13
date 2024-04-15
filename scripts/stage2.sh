76d67d3178f30c3d7d8ecce79a5cb5612b144c66
levrekhlov <rekhlov.lev@mail.ru>
2024-04-15 21:11:24 +0300

    Add draft of stage2.sh

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+password=$(head -n 1 secrets/.hive.pass)
+
+# Run query and save results
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team13 -p $password -f sql/db.hql > output/hive_results.txt 2> /dev/null
