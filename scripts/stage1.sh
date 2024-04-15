0b926855473850edfb1efcabb69a1036ff8b4b4d
Said Kamalov <s.kamalovwork@yandex.ru>
2024-04-15 20:24:48 +0300

    clear `/project/warehouse` if needed

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -18,6 +18,7 @@ python3 scripts/build_projectdb.py
 # Import the database into HDFS via Sqoop
 password=$(head -n 1 secrets/.psql.pass)
 cd output
+hdfs dfs -rm -r /project/warehouse
 sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team13_projectdb --username team13 --password $password --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse --m 1
 
 #
