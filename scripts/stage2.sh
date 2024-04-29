52c391bd9a22719289fd73ffdccc43014b681e4e
levrekhlov <rekhlov.lev@mail.ru>
2024-04-30 02:11:30 +0300

    Redirect q1.hql output to /dev/null.

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -6,4 +6,4 @@ password=$(head -n 1 secrets/.hive.pass)
 beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team13 -p $password -f sql/db.hql > output/hive_results.txt 2> /dev/null
 
 # Q1
-beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team13 -p $password -f sql/q1.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q1.csv
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team13 -p $password -f sql/q1.hql --hiveconf hive.resultset.use.unique.column.names=false > output/q1.csv 2> /dev/null
