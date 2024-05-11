34e947cfef13c3b1f34b44460d414832f5da98d2
levrekhlov <rekhlov.lev@mail.ru>
2024-05-12 00:28:51 +0300

    Add more cleaning to preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Clean everything
+# Clean local files
 rm data/*
 rm output/*
+rm -rf scripts/.ipynb_checkpoints
+rm -rf sql/.ipynb_checkpoints
+
+# Clean hdfs files
 hdfs dfs -rm -r -skipTrash /user/team13/project/warehouse
 hdfs dfs -rm -r -skipTrash /user/team13/project/hive
+hdfs dfs -rm -r -skipTrash /user/team13/project/output
+hdfs dfs -rm -r -skipTrash /user/team13/project/models
