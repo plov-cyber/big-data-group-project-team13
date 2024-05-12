5faca1d5f6a6c564d357e263d1b4dfe7cf6f34c5
levrekhlov <rekhlov.lev@mail.ru>
2024-05-12 14:13:24 +0300

    Add ML results exporting to stage4.sh

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-spark-submit --master yarn scripts/model.py
+spark-submit --master yarn scripts/model.py 1> /dev/null
--- scripts/stage4.sh
+++ scripts/stage4.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+password=$(head -n 1 secrets/.hive.pass)
+
+# Export ML results to Hive tables
+beeline -u jdbc:hive2://hadoop-03.uni.innopolis.ru:10001 -n team13 -p $password -f sql/export_ml_results.hql 2> /dev/null
